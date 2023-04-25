16c427b3434010162f6f71ab73ee651fa2cd0743
kawakibb <kawakibk7@gmail.com>
2023-04-25 14:54:50 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+* print_alphabet - write  all alphabet in lowercase
+* Return: 0 success.
+*/
+
+
+void print_alphabet(void)
+{
+	char alph;
+
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+			_putchar(alph);
+
+	_putchar('\n');
+}

fe36ecd05f383899893d54c79bd3fda6ddfc35d5
kawakibb <kawakibk7@gmail.com>
2023-04-25 15:11:56 -0700

    alphabet

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,12 +1,12 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	void print_alphabet(void);
-	return (0);
-}
+int _putchat(char c);
+void print_alphabet(void);
+#endif

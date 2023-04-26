cecc32aab57eaeafb3939367cfeaa4dc6df9fc1f
kawakibb <kawakibk7@gmail.com>
2023-04-25 17:30:32 -0700

    my commit

--- 0x02-functions_nested_loops/6-main.c
+++ 0x02-functions_nested_loops/6-main.c
@@ -1,15 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _abs - computes the absolute value of an integer
- * @n: the int to check
- * Return: the absolute value of int
- */
-	int _abs(int n)
+* main - check the code
+*
+* Return: Always 0.
+*/
+	int main(void)
 	{
-		if (n >= 0)
-		{
-			return (n);
-		}
-		return (-n);
+	    int r;
+
+	r = _abs(-1);
+	printf("%d\n", r);
+	r = _abs(0);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	r = _abs(-98);
+	printf("%d\n", r);
+	return (0);
 	}
+

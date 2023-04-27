0509caf6e6d954d182a52b307389a3b803e9272f
kawakibb <kawakibk7@gmail.com>
2023-04-27 16:26:21 -0700

    my commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,25 @@
 #include "main.h"
 
 /**
- * _isdigit -  character is digit
+ * print_most_numbers -  character is digit
  * @c: The value of digit
  * Return: 1 if digit or 0 if not
  */
-
-int _isdigit(int c)
+void print_most_numbers(void)
 {
+	char c;
 
-        if (c >= 48 && c <= 57)
-        {
-                return (1);
-        }
-        return (0);
+	for (c = '0'; c <= '9'; c++)
+	{
+	if (c == '2' || c == '4')
+	{
+		
+	}
+	else
+	{
+		_putchar(c);
+	}
+	}
+	_putchar('\n');
+
 }

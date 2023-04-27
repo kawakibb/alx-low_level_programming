d378a54d1aa207eac396b0ba221c26caa3826d9b
kawakibb <kawakibk7@gmail.com>
2023-04-27 11:18:20 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul -  character is digit
+ * @a: The value of first numbre
+ * @b: The value of second number
+ * Return: 1 if digit or 0 if not
+ */
+
+int mul(int a, int b)
+{
+	int x
+	x = a * b;
+	return (x);
+}

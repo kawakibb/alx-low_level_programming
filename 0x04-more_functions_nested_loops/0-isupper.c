2748b7380f878b947d1cb59d8d02aba91eb98862
kawakibb <kawakibk7@gmail.com>
2023-04-27 08:51:27 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper -  letter is upper
+ * @c: The value of the letter
+ * Return: 1 if upper or 0 if not
+ */
+
+int _isupper(int c)
+{
+if (c <= 90)
+{
+	return (1);
+}
+return (0);
+}

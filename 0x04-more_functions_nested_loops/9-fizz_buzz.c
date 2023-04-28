a9b229663826cf5780571b20bc7b6fbaa5991056
kawakibb <kawakibk7@gmail.com>
2023-04-27 18:26:11 -0700

    my commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,25 +9,25 @@
  */
 int main(void)
 {
-	int i;
+	int x;
 
-	for (i = 1; i <= 100; i++)
+	for (x = 1; x <= 100; x++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (x % 3 == 0 && x % 5 != 0)
 		{
 			printf(" Fizz");
-		} else if (i % 5 == 0 && i % 3 != 0)
+		} else if (x % 5 == 0 && x % 3 != 0)
 		{
 			printf(" Buzz");
-		} else if (i % 3 == 0 && i % 5 == 0)
+		} else if (x % 3 == 0 && x % 5 == 0)
 		{
 			printf(" FizzBuzz");
-		} else if (i == 1)
+		} else if (x == 1)
 		{
-			printf("%d", i);
+			printf("%d", x);
 		} else
 		{
-			printf(" %d", i);
+			printf(" %d", x);
 		}
 	}
 	printf("\n");

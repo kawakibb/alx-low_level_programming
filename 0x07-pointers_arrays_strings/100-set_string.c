b271b046502eca5142bbf25d751fc2d655bccfe2
kawakibb <kawakibk7@gmail.com>
2023-05-08 16:40:06 -0700

    Even more pointers commit

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,7 +3,7 @@
 /**
  * set_string - sets the value of a pointer to a char
  * @s: a pointer to an other pointer
- * @to: a character pointer 
+ * @to: a character pointer
  */
 
 void set_string(char **s, char *to)

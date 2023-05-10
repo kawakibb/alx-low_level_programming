ac5e17eeb269025bd0ace249c8ddf8075a5c2aaa
kawakibb <kawakibk7@gmail.com>
2023-05-10 15:54:17 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,25 +6,23 @@
  * @B: pointer second char
  * Return: 0
  */
-
 int wildcmp(char *A, char *B)
 {
-        if (*A == '\0')
-        {
-                if (*B != '\0' && *B == '*')
-                {
-                        return (wildcmp(A, B + 1));
-                }
-                return (*B == '\0');
-        }
-
-        if (*B == '*')
-        {
-                return (wildcmp(A + 1, B) || wildcmp(A, B + 1));
-        }
-        else if (*A == *B)
-        {
-                return (wildcmp(A + 1, B + 1));
-        }
-        return (0);
+	if (*A == '\0')
+	{
+	if (*B != '\0' && *B == '*')
+	{
+	return (wildcmp(A, B + 1));
+	}
+	return (*B == '\0');
+	}
+	if (*B == '*')
+	{
+	return (wildcmp(A + 1, B) || wildcmp(A, B + 1));
+	}
+	else if (*A == *B)
+	{
+	return (wildcmp(A + 1, B + 1));
+	}
+		return (0);
 }

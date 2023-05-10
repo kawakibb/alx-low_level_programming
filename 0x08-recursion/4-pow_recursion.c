9de1ea5d919fed2f96a117a7b4289ba0133f0a95
kawakibb <kawakibk7@gmail.com>
2023-05-10 16:42:48 -0700

    recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+
+/**
+ * _pow_recursion - return x raised to the power of y
+ * @x: nbr duplicated
+ * @y: power of a nbr
+ *
+ * Return: final result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}

a500cd91da1ece4d1aac931085923f21c1cc1678
kawakibb <kawakibk7@gmail.com>
2023-05-12 16:12:36 -0700

    argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - writ the programs name
+ * @argc: number of array arg
+ * @argv: arguments
+ * Return: 0 if Success
+ */
+int main(int argc , char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

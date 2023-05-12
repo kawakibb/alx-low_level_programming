83c2327e4cbfb199f775a8d3322c4a5f686908b7
kawakibb <kawakibk7@gmail.com>
2023-05-12 16:20:58 -0700

    argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - write the number of arguments
+ * @argc: number
+ * @argv: array 
+ *
+ * Return:  0 
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}

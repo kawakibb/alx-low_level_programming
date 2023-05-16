9c678cc2583c4606d995ab74efc995e4717e79e0
kawakibb <kawakibk7@gmail.com>
2023-05-16 16:38:17 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,15 +14,15 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	else if (s2 == NULL)
+	{	s2 = "";
 		i = ci = 0;
+	}
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
-
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;

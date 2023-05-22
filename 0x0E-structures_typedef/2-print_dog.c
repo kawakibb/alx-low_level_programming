8901f38b7d145df71b8c9b0ca31c1ae9354c37f9
kawakibb <kawakibk7@gmail.com>
2023-05-22 15:27:56 -0700

    Structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,10 +11,10 @@
 	if (d == NULL)
 		return;
 
-	if (d.name == NULL)
-		d.name = "(nil)";
-	if (d.owner == NULL)
-		d.owner = "(nil)";
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d.name, d.age, d.owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }

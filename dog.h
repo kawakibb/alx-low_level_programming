c6b860bf8bbd463a22e5e243450c058c49cd2522
kawakibb <kawakibk7@gmail.com>
2023-05-22 14:38:19 -0700

    Structures

---
+++ dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - dog informtion
+ * @name: First index
+ * @age: Second index
+ * @owner: Third index
+ *
+ * Description: description
+ */
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+#endif

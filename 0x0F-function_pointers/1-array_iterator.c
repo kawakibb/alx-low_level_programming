4b926176f30427ac5e75e3e73d840207bb6aea82
kawakibb <kawakibk7@gmail.com>
2023-05-24 15:13:14 -0700

    Structures

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* array_iterator - prints array element
+* @array: array as pinter
+* @size: zine of nbrs to print
+* @action: pointer
+* Return: nthing
+*/
+	void array_iterator(int *array, size_t size, void (*action)(int))
+	{
+		unsigned int i;
+
+
+		if (array == NULL || action == NULL)
+			return;
+
+
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}

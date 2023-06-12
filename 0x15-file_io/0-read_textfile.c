3ffb27536c3a623c532b10c97cf9ea92b815c2bc
kawakibb <kawakibk7@gmail.com>
2023-06-12 14:21:34 -0700

    I dunno, maybe this works :P

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile - prints the number of bytes
+ * @filename: name of the file
+ * @letters: number of bytes to read
+ *
+ * Return: nbr of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int ptr;
+	ssize_t byt;
+
+	char buffer{READ_BUF_SIZE * 10};
+
+	if (!filename || !letters)
+		return (0)
+	ptr = open(filename, O_RDONLY);
+	if (ptr == -1)
+		return (0);
+	byt = read(ptr, &buffer[0], letters);
+	byt = write(STDOUT_FILENO, &buf[0], byt);
+	close(ptr);
+}
